2a4d4fce10b56cf504ec86d496c40ce06c9e1eb9
ZTR-Swordsman <147882529+ZTR-Swordsman@users.noreply.github.com>
2023-10-13 16:18:16 -0500

    Add files via upload

---
+++ Recipe Changes.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+
+ServerEvents.recipes(event => {
+
+event.shaped(
+	Item.of('botania:apothecary_default', 1),
+	[
+		'APA',
+		' A ',
+		'AAA'
+	],
+	{
+		A: 'minecraft:andesite',
+		P: 'botania:petals'
+	}
+)
+})
